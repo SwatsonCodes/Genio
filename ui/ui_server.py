34ffc79e63cae43968dc554f87e9a5ce8fec050d
SwatsonCodes <samuelpwatson@gmail.com>
2015-10-28 21:32:03 -0600

    main method for ui server.

--- ui/ui_server.py
+++ ui/ui_server.py
@@ -1,11 +1,9 @@
 import http.server
 import socketserver
 
-PORT = 9080
-
-Handler = http.server.SimpleHTTPRequestHandler
-
-httpd = socketserver.TCPServer(("", PORT), Handler)
-
-print("serving UI at port", PORT)
-httpd.serve_forever()
+if __name__ == '__main__':
+    PORT = 9080
+    Handler = http.server.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(("", PORT), Handler)
+    print("serving UI at port", PORT)
+    httpd.serve_forever()
