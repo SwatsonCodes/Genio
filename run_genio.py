305875fd6614962d9556ce2df3756659b959e9e1
SwatsonCodes <samuelpwatson@gmail.com>
2015-10-30 10:13:37 -0600

    improved run script and speedier (looking) load animation.

--- run_genio.py
+++ run_genio.py
@@ -1,5 +1,11 @@
 import subprocess
+import sys
 
-servers = [subprocess.Popen(['python', './api_server.py']), subprocess.Popen(['python', './ui/ui_server.py'])]
-for s in servers:
-    s.wait()
+if __name__ == '__main__':
+    pyname = 'python'
+    if sys.version_info[0] < 3:
+        pyname += '3'
+
+    servers = [subprocess.Popen([pyname, './api_server.py']), subprocess.Popen([pyname, './ui/ui_server.py'])]
+    for s in servers:
+        s.wait()
